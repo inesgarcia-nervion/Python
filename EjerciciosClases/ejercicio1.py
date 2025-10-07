62242b41a6cf4ed211139fb905b10d4131a2bb7b
inesgarcia-nervion <ines.garcia@iesnervion.es>
2025-10-07 14:40:04 +0200

    Ejercicio 1 clases

--- EjerciciosClases/ejercicio1.py
+++ EjerciciosClases/ejercicio1.py
@@ -11,6 +11,31 @@ Añade los siguientes constructores:
     c.Crear también los métodos __str__, __eq__ y __lt__. Se considera que dos cuentas corrientes son iguales
     si tienen el mismo DNI. Las cuentas corrientes se ordenarán de menor a mayor por el saldo. '''
 
+class CuentaCorriente:
+    def __init__(self, dni, saldo_inicial, nombre=""):     #Constructores (nombre tiene que estar vacio)
+        self.dni = dni
+        self.saldo = saldo_inicial
+        self.nombre = nombre
 
+    
+    def sacarDinero(self, cantidad):  #Método para sacar dinero
+        if cantidad <= self.saldo:
+            self.saldo -= cantidad
+            return True
+        else:
+            return False
+        
+        
+    def ingresarDinero(self, cantidad):    #Método para ingresar dinero
+        if cantidad >  0:
+            self.saldo += cantidad
+            return True
+        else:
+            return False
+    
 
-
+    def __str__(self):  #Método str
+        return f"DNI: {self.dni}, Nombre: {self.nombre}, Saldo: {self.saldo}"
+    
+    def __eq__(self, other): #Método eq
+        return self.dni == other.dni
