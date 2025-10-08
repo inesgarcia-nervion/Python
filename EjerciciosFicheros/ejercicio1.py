b01c51e6441db93ac1ad46ca5aa4bb6bf96dea8a
inesgarcia-nervion <ines.garcia@iesnervion.es>
2025-10-08 10:17:19 +0200

    Ejercicio 1 fichero

--- EjerciciosFicheros/ejercicio1.py
+++ EjerciciosFicheros/ejercicio1.py
@@ -6,3 +6,26 @@ de los alumnos de un grupo, cada uno en una línea:
     …
 Implementa un programa que lea del fichero los datos, muestre los nombres y calcule la media de la edad
 y de las estaturas, mostrándolas por pantalla.   '''
+
+
+
+f = open('Alumnos.txt', 'w', encoding="utf8")   #Para que se vean las tildes
+f.write("juan 22 1.77 \n")
+f.write("luis 21 1.80 \n")
+f.write("pedro 20 1.73 \n")
+
+
+
+class Alumno:
+    def __init__(self, nombre, edad, estatura):
+        self.nombre = nombre
+        self.edad = edad
+        self.estatura = estatura
+    
+    def leerNombres(self):
+        alumnos = []
+        f = open('Alumnos.txt', 'w', encoding="utf8")
+        for linea in f:
+            letras = linea.split()
+
+
