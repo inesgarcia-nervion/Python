6b0751810a0710424fe89a7830add3a83902e80c
inesgarcia-nervion <ines.garcia@iesnervion.es>
2025-10-10 20:34:41 +0200

    Ejercicios 3 y 4 ficheros

--- EjerciciosFicheros/ejercicio3.py
+++ EjerciciosFicheros/ejercicio3.py
@@ -1,3 +1,11 @@
-''' 3. Diseña una aplicación que pida al usuario su nombre y edad. Estos datos deben guardarse en el fichero
-datos.txt. Si este fichero existe, deben añadirse al final en una nueva línea, y en caso de no existir, debe
-crearse. '''
+''' 3. Diseña una aplicación que pida al usuario su nombre y edad. Estos datos deben 
+guardarse en el fichero datos.txt. Si este fichero existe, deben añadirse al final 
+en una nueva línea, y en caso de no existir, debe crearse. '''
+
+
+nombre = input("Introduzca su nombre: ")
+edad = int(input("Introduzca su edad: "))
+
+
+with open ('datos.txt', 'a', encoding="utf8") as a:         #Abriendo el archivo con append, nos añade los datos si el fichero existe y sino lo crea
+    a.write(f'Nombre: {nombre} , Edad: {edad} \n' )
