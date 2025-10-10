edae163923f04cbe64000a9675c9fe9fc1e07c02
inesgarcia-nervion <ines.garcia@iesnervion.es>
2025-10-10 19:48:27 +0200

    Ejercicio 1 y 2 ficheros

--- EjerciciosFicheros/ejercicio1.py
+++ EjerciciosFicheros/ejercicio1.py
@@ -9,23 +9,35 @@ y de las estaturas, mostrándolas por pantalla.   '''
 
 
 
-f = open('Alumnos.txt', 'w', encoding="utf8")   #Para que se vean las tildes
-f.write("juan 22 1.77 \n")
-f.write("luis 21 1.80 \n")
-f.write("pedro 20 1.73 \n")
+with open('Alumnos.txt', 'w', encoding="utf8") as w:   #Se escriben los datos
+    w.write("juan 22 1.77 \n")
+    w.write("luis 21 1.80 \n")
+    w.write("pedro 20 1.73 \n")
 
 
+calcularEdad = 0
+calcularEstatura = 0
+totalAlumnos = 0
 
-class Alumno:
-    def __init__(self, nombre, edad, estatura):
-        self.nombre = nombre
-        self.edad = edad
-        self.estatura = estatura
-    
-    def leerNombres(self):
-        alumnos = []
-        f = open('Alumnos.txt', 'w', encoding="utf8")
-        for linea in f:
-            letras = linea.split()
+
+with open('Alumnos.txt', 'r', encoding="utf8") as r:        #Se leen los datos
+    for linea in r:
+        separacion = linea.split()
+        nombre = separacion[0]
+        edad = int(separacion[1])
+        estatura = float(separacion[2])
+
+        print(nombre)                           #Hay que imprimir los nombres dentro del bucle
+
+        calcularEdad += edad
+        calcularEstatura += estatura
+        totalAlumnos += 1
+
+mediaEdad = calcularEdad / totalAlumnos
+mediaEstatura = calcularEstatura / totalAlumnos
+
+print(f'La media de edad de los alumnos es de {mediaEdad} ')
+print(f'La media de la estatura de los alumnos es de {mediaEstatura} ')
+
 
 
--- EjerciciosFicheros/ejercicio2.py
+++ EjerciciosFicheros/ejercicio2.py
@@ -1,3 +1,21 @@
 ''' 2.Crea un programa en python que cree un fichero en modo escritura. A continuación, irá leyendo línea
 a línea de teclado hasta que el usuario introduzca la cadena “fin”. Debe escribir cada línea en el fichero
 creado. '''
+
+fin = False
+listaCompleta = ""
+
+    
+while not fin:
+    palabras = input("Introduzca palabras al azar (Escriba fin cuando quiera dejar el programa): ")
+    if palabras.lower() == "fin":
+        print("Fin del programa")
+        fin = True
+        
+    else:
+        listaCompleta += palabras + "\n"
+
+
+with open('Palabras.txt', 'w', encoding="utf8") as w:   #Se escriben los datos
+    w.write(listaCompleta)
+    print(listaCompleta)
